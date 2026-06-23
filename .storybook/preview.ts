d05a6da29f7f7d888357afe13c20304e4b97c8c3
Jiro Ghianni <118177951+jiromaykin@users.noreply.github.com>
2026-06-23 14:38:52 +0200

    :lipstick: fix(plan-card): improve storybook decorators

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,10 @@
 import type { Preview } from '@storybook/web-components-vite';
+import { html } from 'lit';
 
 const preview: Preview = {
+  decorators: [(story) => html` <div style="max-inline-size: 600px; padding: 2rem;">${story()}</div> `],
   parameters: {
+    layout: 'padded',
     controls: {
       matchers: {
         color: /(background|color)$/i,
